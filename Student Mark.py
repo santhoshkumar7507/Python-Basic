f55e024b366e5904494a60d1c893c33c6297203b
santhoshkumar7507 <ssksanthoshkumar1433@gmail.com>
2026-06-15 12:59:29 +0530

    Student mark

---
+++ Student Mark.py
@@ -0,0 +1,12 @@
+marks = {
+    "Math": 85,
+    "Science": 90,
+    "English": 80
+}
+
+total = sum(marks.values())
+
+average = total / len(marks)
+
+print("Total:", total)
+print("Average:", average)
